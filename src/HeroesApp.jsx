8f5321b007c2a685ad3bcc096948045c7def2c9b
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-08-18 21:33:12 +0000

    hero update

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,8 +1,11 @@
-import { AppRouter } from './router/AppRouter.jsx'
+import { AppRouter } from './router/AppRouter.jsx';
+import { AuthProvider } from './auth';
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
+      
       <AppRouter />
-    </>)
+      
+    </AuthProvider>)
 }
--- src/heroes/pages/Search.jsx
+++ src/heroes/pages/Search.jsx
@@ -1,6 +1,6 @@
 import { HeroCard } from '../components';
 import { useForm } from '../../hooks';
-import {getHeroByName} from '../helpers';
+import { getHeroByName } from '../helpers';
 
 import { useNavigate, useLocation } from 'react-router-dom';
 import querySring from 'query-string';
@@ -8,7 +8,7 @@ import querySring from 'query-string';
 export const Search = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const { q='' } = querySring.parse(location.search);
+  const { q = '' } = querySring.parse(location.search);
   const heroes = getHeroByName(q);
 
   const { searchText, onInputChange } = useForm({
@@ -17,7 +17,7 @@ export const Search = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    /*if (searchText.trim().length <= 1) return;*/
     navigate(`?q=${searchText}`)
   }
 
@@ -29,7 +29,7 @@ export const Search = () => {
       <div className="row">
 
         <div className="col-5">
-          <h4>Hero Searching Tool</h4>
+          <code>Try to write the name of the hero or maybe just a part ;) </code>
           <hr />
           <form onSubmit={onSearchSubmit}>
 
@@ -50,15 +50,23 @@ export const Search = () => {
         <div className="col-7">
           <h4> Results </h4>
           <hr />
-          <div className="alert alert-primary">
-            Search a Hero
-          </div>
-          <div className="alert alert-danger">
-            There's no results for <b>{q}</b>
-          </div>
+          {
+            q === '' ? <div
+              className="alert alert-primary 
+                         animate__animated animate__fadeIn">
+              Search a Hero :D</div> :
+              heroes <= 0 && <div
+                className="alert alert-danger
+                           animate__animated animate__fadeIn">
+                There's no results for <b>{q}</b> :'c
+              </div>
+          }
+
+
+
           {
             heroes.map(hero => (
-              <HeroCard key={hero.id} {...hero}/>))
+              <HeroCard key={hero.id} {...hero} />))
           }
         </div>
       </div>

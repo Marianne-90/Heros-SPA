6eaa904f1a04b253e5656298e5c7ddddc84d38be
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-08-19 16:17:30 +0000

    final changes

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../context';
 
 export const LoginPage = () => {
-  
-const navigate = useNavigate();
-  
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const onLogin = () => {
-    navigate('/marvel',{replace:true})
-  }
+
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login('Marianne G.');
+
+    navigate(lastPath, { replace: true })
+  };
+
   return (
     <>
       <div className="container mt-5">
